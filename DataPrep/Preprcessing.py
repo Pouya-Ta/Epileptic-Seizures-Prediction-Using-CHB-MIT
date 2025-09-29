0a073857fe5e299e7b9659a6e2f73b4cd5a66628
Pouya Taghipour <87365616+Pouya-Ta@users.noreply.github.com>
2025-09-29 13:15:05 +0330

    Update Preprcessing.py

--- DataPrep/Preprcessing.py
+++ DataPrep/Preprcessing.py
@@ -2,3 +2,12 @@ import os, re, json, math, random, warnings
 from pathlib import Path
 from typing import List, Tuple, Dict, Any, OrderedDict as TOrdered
 warnings.filterwarnings("ignore", category=RuntimeWarning)
+
+import numpy as np
+import matplotlib
+matplotlib.use("Agg")  # headless
+import matplotlib.pyplot as plt
+
+import mne
+from mne.preprocessing import ICA
+import json, numpy as np
