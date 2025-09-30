75a554ea6f982543e856dd3069f12cdd9c9e2dba
Pouya Taghipour <87365616+Pouya-Ta@users.noreply.github.com>
2025-09-30 21:20:58 +0330

    Update Preprcessing.py

--- DataPrep/Preprcessing.py
+++ DataPrep/Preprcessing.py
@@ -11,3 +11,8 @@ import matplotlib.pyplot as plt
 import mne
 from mne.preprocessing import ICA
 import json, numpy as np
+
+# -----------------------------------------------------------------------------
+# --------------------------- USER CONFIG (edit) -------------------------------
+# -----------------------------------------------------------------------------
+DATASET_ROOT = r"D:\Project\CHB-MIT\Original_dataset\CHB-MIT"  # your input root
