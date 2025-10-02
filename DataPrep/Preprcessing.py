230e74b30e227814b59c8c4a191556818b0b2c72
Pouya Taghipour <87365616+Pouya-Ta@users.noreply.github.com>
2025-10-02 17:43:38 +0330

    Update Preprcessing.py

--- DataPrep/Preprcessing.py
+++ DataPrep/Preprcessing.py
@@ -42,3 +42,4 @@ NORMALIZE  = "relative"  # "none" | "relative" | "log" | "zscore"
 # window policy
 WIN_LEN_S    = 2.0       # 2-second windows
 WIN_STRIDE_S = 2.0       # non-overlapping
+PRE_ICTAL_START_OFFSET = 40*60  # 40 min before onset
